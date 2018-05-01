be10fc8256515d6e21855460a676dd0c18e71bdb
Vladislav <vladislav.ykshn@gmail.com>
2018-05-01 13:52:14 +0300

    VALUE -> VALUES

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,14 +9,14 @@ VALUES('2', 'Second Task', 'Some description', null);
  */
 
 INSERT INTO USER
-VALUE ('1', 'Vladislav', 'USER');
+VALUES('1', 'Vladislav', 'USER');
 
 INSERT INTO CREDENTIALS
-VALUE ('1', 'USER', '123', '1');
+VALUES('1', 'USER', '123', '1');
 
 
 INSERT INTO USER
-VALUE ('2', 'Denis', 'ADMIN');
+VALUES('2', 'Denis', 'ADMIN');
 
 INSERT INTO CREDENTIALS
-VALUE ('2', 'ADMIN', '123', '2');
+VALUES('2', 'ADMIN', '123', '2');
